4f210ad07344bf97dc76392007a5f9aa7244160a
0xdonnie7 <ddcollection555@gmail.com>
2026-06-23 09:36:24 +0100

    feat(auth): add logic to signup handler

--- cmd/api/auth_handler.go
+++ cmd/api/auth_handler.go
@@ -3,20 +3,63 @@ package main
 import (
 	"net/http"
 
+	"github.com/0xdonnie7/Expense_API/internal/auth"
+	"github.com/0xdonnie7/Expense_API/internal/database"
 	"github.com/0xdonnie7/Expense_API/validator"
+	"github.com/google/uuid"
 )
 
 func (app *application) signupHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := readJSON(w, r, &input); err != nil {
+		app.invalidJSONResponse(w, err)
+		return
+	}
+
+	v := validator.New()
+	v.ValidateUser(input.Email, input.Password)
+	if !v.Valid() {
+		app.failedValidationResponse(w, v.Errors)
+	}
+
+	password := auth.Password{}
+
+	err := password.HashPassword(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	user, err := app.db.CreateUser(r.Context(), database.CreateUserParams{
+		ID:           uuid.New(),
+		Email:        input.Email,
+		PasswordHash: password.Hash,
+	})
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = writeJSON(w, http.StatusOK, user)
+	if err != nil {
+		app.invalidJSONResponse(w, err)
+	}
+
+}
+
+func (app *application) loginHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Email    string
 		Password string
 	}
 
-	if err := readJSON(w, r, &input); err != nil {
+	err := readJSON(w, r, &input)
+	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
-
-	v := validator.New()
-	v.Check(input.Email != "", "email", "must not be empty")
 }
