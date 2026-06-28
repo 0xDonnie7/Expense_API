b450dbd63d383b6e4187a77f1142a99a99155756
0xdonnie7 <ddcollection555@gmail.com>
2026-06-28 10:02:46 +0100

    refactor(routes): simplify endpoints

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -4,25 +4,25 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	_ "github.com/lib/pq"
 )
 
 func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
 
 	r.Group(func(r chi.Router) {
-		r.Post("auth/signup", app.signupHandler)
-		r.Post("auth/login", app.loginHandler)
+		r.Post("/auth/signup", app.signupHandler)
+		r.Post("/auth/login", app.loginHandler)
 	})
 
 	r.Group(func(r chi.Router) {
 		r.Use(app.authRequired)
 
-		r.Route("api/expense", func(r chi.Router) {
-			r.Post("/create", app.createExpense)
-			// r.Delete("/remove", app.removeExpense)
+		r.Route("/api/expense", func(r chi.Router) {
+			r.Post("/", app.createExpense)
 		})
 
-		r.Route("api/expense/{id}", func(r chi.Router) {
+		r.Route("/api/expense/{id}", func(r chi.Router) {
 			r.Get("/", app.getExpenseByID)
 			r.Put("/", app.updateExpenseByID)
 			r.Delete("/", app.removeExpenseByID)
